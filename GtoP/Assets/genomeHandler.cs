d4b55120767c0d9a3a87c0a76f8a6c7061acaacb
Gabriel <gabriel.dellaccio@gmail.com>
2017-07-21 11:18:48 -0400

    Debugged and finished genome creation, mutation, and deletion

--- GtoP/Assets/genomeHandler.cs
+++ GtoP/Assets/genomeHandler.cs
@@ -11,21 +11,6 @@ public class genomeHandler : MonoBehaviour {
 
 	System.Random rand = new System.Random();
 
-	public int maxSpawn = 100;
-	public int vMax = 5;
-	public int vDurationMin = 1;
-	public int vDurationMax = 100;
-	public int gMax = 3;
-	public int gDurationMin = 1;
-	public int gDurationMax = 100;
-
-	public float dupeRate = 0.05f;
-	public float muteRate = 0.05f;
-	public float delRate = 0.01f;
-	public float changePercent = 0.15f;
-
-	// Number of genes in a genom
-	public float numGenes = 10;
 
 	////// STRUCT: gene
 	// 	This structure represents a single gene.
@@ -34,15 +19,7 @@ public class genomeHandler : MonoBehaviour {
 	//			INPUT: int maxSpawn,  int vMax, int vDurationMin, int vDurationMax, int gMax, int gDurationMin, int gDurationMax, int index
 	//			OUTPUT: a gene with --> (partType, angle, startTime, velocity, traveltime, growthRate, growthTime, index).
 	public struct gene {
-
-		System.Random rand;
-		// public int maxSpawn;
-		// public int vMax;
-		// public int vDurationMin;
-		// public int vDurationMax;
-		// public int gMax;
-		// public int gDurationMin;
-		// public int gDurationMax;
+		
 
 		public double x;
 		public double y;
@@ -57,17 +34,17 @@ public class genomeHandler : MonoBehaviour {
 		public float growthTime;// # 6 = Growth Time
 		public float index;// # 7 = Index
 
-		public void setGeneParameters(int maxSpawn,  int vMax, int vDurationMin, int vDurationMax, int gMax, int gDurationMin, int gDurationMax, int index) {
-			rand = new System.Random();
 
-			partType = rand.Next(0, 4);
-			angle = rand.Next(0, 360);
-			startTime = rand.Next(0, maxSpawn);
-			velocity = rand.Next(1, vMax);
-			travelTime = rand.Next(vDurationMin, vDurationMax);
-			growthRate = rand.Next(1, gMax);
-			growthTime = rand.Next(gDurationMin, gDurationMax);
-			index = index;
+		public void setGeneParameters(int pt,  int ang, int sT, int vel, int tT, int gR, int gT, int geneNumber) {
+			// Debug.Log("GENES ARE BEING SET");
+			partType = pt;
+			angle = ang;
+			startTime = sT;
+			velocity = vel;
+			travelTime = tT;
+			growthRate = gR;
+			growthTime = gT;
+			index = geneNumber;
 		}	
 	}
 
@@ -79,12 +56,12 @@ public class genomeHandler : MonoBehaviour {
 	// 		duplicateAndDelete() : Duplicates and deletes genes in the genome.
 	// 		mutate() : Mutates genes in the genome.
 	public struct genome {
-		System.Random rand;
+		// System.Random rand;
 
 		public gene[] arrayOfGenes;
 		public gene[] newMutatedGenome;
 
-		public int numberOfGenesInGenome;
+		public int numberOfGenes;
 		public float dupeRate;
 		public float muteRate;
 		public float delRate;
@@ -93,27 +70,40 @@ public class genomeHandler : MonoBehaviour {
 
 		public int numberOfDuplications;
 
+
+		System.Random rand;
+		public void createRandomFunction() {
+			rand = new System.Random();
+		}
+
 		// Function to set the genome parameters.
-		public void setGenomeParameters(int numberOfGenesInGenome, float dupeRate, float muteRate, float delRate, float changePercent) {
-			rand = new System.Random(); 
-			numberOfGenesInGenome = numberOfGenesInGenome;
-			dupeRate = dupeRate;
-			muteRate = muteRate;
-			delRate = delRate;
-			changePercent = changePercent;	
+		public void setGenomeParameters(int numberOfGenesInGenome, float dupR, float muteR, float delR, float changeP) {
+			numberOfGenes = numberOfGenesInGenome;
+			arrayOfGenes = new gene[numberOfGenesInGenome];
+			dupeRate = dupR;
+			muteRate = muteR;
+			delRate = delR;
+			changePercent = changeP;	
 			numberOfDuplications = 0;
 			firstDupe = true;
+			// Debug.Log("PARAMETERS SET");
 		}
 
 		// Creates the specified genome after setting parameters.
 		public void createWholeGenome(int maxSpawn, int vMax, int vDurationMin, int vDurationMax, int gMax, int gDurationMin, int gDurationMax) {
-			arrayOfGenes = new gene[numberOfGenesInGenome];
-			// Use i for indexing the genes in the array of genes. 
-			int i = 0;
-			for (int j = 0; j < numberOfGenesInGenome; j++) {
-				arrayOfGenes[j].setGeneParameters(100, 5, 1, 100, 3, 1, 100, i);
-				i = i + 1;
+			Debug.Log("Number of genes: " +numberOfGenes);
+			for (int j = 0; j < numberOfGenes; j++) {
+				int partType = rand.Next(0,4);
+				int angle = rand.Next(0, 360);
+				int startTime =  rand.Next(0, maxSpawn);
+				int velocity = rand.Next(1, vMax);
+				int travelTime = rand.Next(vDurationMin, vDurationMax);
+				int growthRate = rand.Next(1, gMax);
+				int growthTime = rand.Next(gDurationMin, gDurationMax);
+				// maxSpawn,  int vMax, int vDurationMin, int vDurationMax, int gMax, int gDurationMin, int gDurationMax, int geneNumber
+				arrayOfGenes[j].setGeneParameters(partType, angle, startTime, velocity, travelTime, growthRate, growthTime, j);
 			}
+			// Debug.Log("GENOME CREATED");
 		}
 
 		// Duplicates and deletes the different genes in a genome.
@@ -143,34 +133,40 @@ public class genomeHandler : MonoBehaviour {
 			}
 			arrayOfGenes = geneDuplicatedAndNotDeleted.ToArray();
 			int lengthOfNewArray = arrayOfGenes.Length;
-		
 			for (int j = 0; j < lengthOfNewArray; j++) {
-				arrayOfGenes[j].index = j;
-				
+				arrayOfGenes[j].index = j;	
 			}
 		}
 
 		// Mutates the genes in the genome.
 		public void mutate() {
 			int lengthOfArray = arrayOfGenes.Length;
+			Debug.Log("MUTE RATE: "+ muteRate);
 			for (int j = 0; j < lengthOfArray; j++) {
 				for (int i = 0; i < 7; i++) {
 					if (rand.NextDouble() <= muteRate) {
 						if (i == 0) {
 							arrayOfGenes[j].partType = rand.Next(0,4);
+							// Debug.Log("THERE HAS BEEN A MUTATION");
 						} else if (rand.NextDouble() > 0.5) {
 							if (i == 1) {
 								arrayOfGenes[j].angle = arrayOfGenes[j].angle + arrayOfGenes[j].angle*changePercent;
+								// Debug.Log("THERE HAS BEEN A MUTATION");
 							} else if (i == 2) {
 								arrayOfGenes[j].startTime = arrayOfGenes[j].startTime  + arrayOfGenes[j].startTime *changePercent;
+								// Debug.Log("THERE HAS BEEN A MUTATION");
 							} else if (i == 3) {
 								arrayOfGenes[j].velocity = arrayOfGenes[j].velocity + arrayOfGenes[j].velocity*changePercent;
+								// Debug.Log("THERE HAS BEEN A MUTATION");
 							} else if (i == 4) {
 								arrayOfGenes[j].travelTime = arrayOfGenes[j].travelTime + arrayOfGenes[j].travelTime*changePercent;
+								// Debug.Log("THERE HAS BEEN A MUTATION");
 							} else if (i == 5) {
 								arrayOfGenes[j].growthRate = arrayOfGenes[j].growthRate + arrayOfGenes[j].growthRate*changePercent;
+								// Debug.Log("THERE HAS BEEN A MUTATION");
 							} else if (i == 6) {
 								arrayOfGenes[j].growthTime = arrayOfGenes[j].growthTime + arrayOfGenes[j].growthTime*changePercent;
+								// Debug.Log("THERE HAS BEEN A MUTATION");
 							} else if (i == 7) {
 								Debug.Log(arrayOfGenes[j].index);
 							}
@@ -179,6 +175,32 @@ public class genomeHandler : MonoBehaviour {
 				}
 			}
 		}
+
+		public void printGenomeContents() {
+			int lengthOfArray = numberOfGenes;
+			string printString = "";
+			for (int j = 0; j < lengthOfArray; j++) {
+				printString += "|| ID NUMBER: " + arrayOfGenes[j].index;
+				printString += ", Part type: "+arrayOfGenes[j].partType;
+				printString += ", Angle: "+arrayOfGenes[j].angle;
+				printString += ", Start time: "+arrayOfGenes[j].startTime;
+				printString += ", Velocity: "+arrayOfGenes[j].velocity;
+				printString += ", Travel time: "+arrayOfGenes[j].travelTime;
+				printString += ", Growth rate: "+arrayOfGenes[j].growthRate;
+				printString += ", Growth time: "+arrayOfGenes[j].growthTime;
+
+				// Debug.Log("PART TYPE: "+arrayOfGenes[j].partType);// 1
+				// Debug.Log("ANGLE: "+arrayOfGenes[j].angle);// 2
+				// Debug.Log("START TIME: "+arrayOfGenes[j].startTime);// 3
+				// Debug.Log("VELOCITY: "+arrayOfGenes[j].velocity);// 4
+				// Debug.Log("TRAVEL TIME: "+arrayOfGenes[j].travelTime);// 5
+				// Debug.Log("GROWTH RATE: "+arrayOfGenes[j].growthRate);// 6
+				// Debug.Log("GROWTH TIME: "+arrayOfGenes[j].growthTime);// 7
+				// Debug.Log("INDEX: "+arrayOfGenes[j].index);// 8
+			}
+			Debug.Log(printString);
+		}
+
 	}
 
 	////// STRUCT: generation
@@ -248,8 +270,17 @@ public class genomeHandler : MonoBehaviour {
 	}
 
 
+	////// STRUCT: genomeToPhenotype
+	// 	This structure represents a single generation, or a collection of genomes (individuals).
+	// 	Functions:
+	// 		setGenerationParameters() : specifies number of genes.
+	// 			INPUT: (int meanNumberOfGenes, int meanStandardDeviationOfGenes, int numberOfIndividualsInGeneration)
+	//			OUTPUT: assigns the generation's parameters.
+	//		createStartGeneration() : creates a generation given the parameters set above.
+	// 		createNumberOfGenes() : creates the number of genes for a given genome in the generation.
+	public struct genomeToPhenotype {
 
-	public struct genomeToPhenotypeParams {
+		List<float[]> arrayList;
 
 		public genome givenGenome; 
 
@@ -281,14 +312,16 @@ public class genomeHandler : MonoBehaviour {
 
 		public void devoGraphics() {
 
-		//0 public float partType;// # 0 = Part Type (0 = IR, 1 = Photo, 2 = Neuron, 3 = R Motor, 4 = L Motor)
-		//1 public float angle;// # 1 = Angle
-		//2 public float startTime;// # 2 = Start Time
-		//3 public float velocity;// # 3 = Velocity
-		//4 public float travelTime;// # 4 = Travel Time
-		//5 public float growthRate;// # 5 = Growth Rate
-		//6 public float growthTime;// # 6 = Growth Time
-		//7 public float index;// # 7 = Index
+			arrayList = new List<float[]>();
+
+			//0 public float partType;// # 0 = Part Type (0 = IR, 1 = Photo, 2 = Neuron, 3 = R Motor, 4 = L Motor)
+			//1 public float angle;// # 1 = Angle
+			//2 public float startTime;// # 2 = Start Time
+			//3 public float velocity;// # 3 = Velocity
+			//4 public float travelTime;// # 4 = Travel Time
+			//5 public float growthRate;// # 5 = Growth Rate
+			//6 public float growthTime;// # 6 = Growth Time
+			//7 public float index;// # 7 = Index
 
 			for (int j = 0; j < genomeLength; j++) {
 				if (givenGenome.arrayOfGenes[j].startTime <= count) {
@@ -306,14 +339,27 @@ public class genomeHandler : MonoBehaviour {
 						size =  (1 + givenGenome.arrayOfGenes[j].growthRate * givenGenome.arrayOfGenes[j].growthTime);
 					}
 
-					givenGenome.arrayOfGenes[j].x = x;
-					givenGenome.arrayOfGenes[j].y = y;
-					givenGenome.arrayOfGenes[j].size = size;
+					float[] tempArray = new float[4];
+					tempArray[0] = (float)x;
+					tempArray[1] = (float)y;
+					tempArray[2] = size;
+					tempArray[3] = givenGenome.arrayOfGenes[j].index;
+					// givenGenome.arrayOfGenes[j].x = x;
+					// givenGenome.arrayOfGenes[j].y = y;
+					// givenGenome.arrayOfGenes[j].size = size;
+					arrayList.Add(tempArray);
+
 				}
 				// givenGenome[j].setGeneParameters(100, 5, 1, 100, 3, 1, 100, i);
 			}
+			int lengthfList = arrayList.Count;
+
+			// for (int i = 0; i < lengthfList; i ++) {
+			// 	Debug.Log(arrayList[i]);
+			// }
 
 		}
+
 
 
 
@@ -323,7 +369,29 @@ public class genomeHandler : MonoBehaviour {
 
 	}
 
-	
+	////// STRUCT: createParams
+	// 	This structure represents a single generation, or a collection of genomes (individuals).
+	// 	Functions:
+	// 		setGenerationParameters() : specifies number of genes.
+	// 			INPUT: (int meanNumberOfGenes, int meanStandardDeviationOfGenes, int numberOfIndividualsInGeneration)
+	//			OUTPUT: assigns the generation's parameters.
+	//		createStartGeneration() : creates a generation given the parameters set above.
+	// 		createNumberOfGenes() : creates the number of genes for a given genome in the generation.
+	public struct createParams {
+		int RMI;
+		int LMI;
+		int NUM_INPUT;
+		int NUM_HIDDEN;
+		int NUM_OUTPUT;
+
+		float[,] TESTinput_to_output;
+		float[,] TESTinput_to_hidden;
+		float[,] TESThidden_to_hidden;
+		float[,] TESThidden_to_output;
+		float[,] TESToutput_to_hidden;
+		int[] rmiVal;
+		int[] lmiVal;	
+	}
 
 
 	// Use this for initialization
@@ -331,23 +399,60 @@ public class genomeHandler : MonoBehaviour {
 
 		Debug.Log("START");
 
-		float[,] TESTinput_to_output = new float[4,2] {{0,0},{0,0},{0,0},{0,0}};
-		float[,] TESTinput_to_hidden = new float[4,6] {{0,0,1,-1,0,0},{0,0,-1,1,0,0},{1,-1,0,0,0,0},{-1,1,0,0,0,0}};
-		float[,] TESThidden_to_hidden = new float[6,6] {{0,0,0,0,1,0},{0,0,0,0,1,0},{0,0,0,0,0,1},{0,0,0,0,0,1},{0,0,0,0,0,0},{0,0,0,0,0,0}};
-		float[,] TESThidden_to_output = new float[6,2] {{0,0},{0,0},{0,0},{0,0},{1,-0.5f},{-0.5f,1}};
-		float[,] TESToutput_to_hidden = new float[2,6] {{0,0,0,0,0,0},{0,0,0,0,0,0}};
+		// float[,] TESTinput_to_output = new float[4,2] {{0,0},{0,0},{0,0},{0,0}};
+		// float[,] TESTinput_to_hidden = new float[4,6] {{0,0,1,-1,0,0},{0,0,-1,1,0,0},{1,-1,0,0,0,0},{-1,1,0,0,0,0}};
+		// float[,] TESThidden_to_hidden = new float[6,6] {{0,0,0,0,1,0},{0,0,0,0,1,0},{0,0,0,0,0,1},{0,0,0,0,0,1},{0,0,0,0,0,0},{0,0,0,0,0,0}};
+		// float[,] TESThidden_to_output = new float[6,2] {{0,0},{0,0},{0,0},{0,0},{1,-0.5f},{-0.5f,1}};
+		// float[,] TESToutput_to_hidden = new float[2,6] {{0,0,0,0,0,0},{0,0,0,0,0,0}};
 
-		int[] rmiVal = new int[1] {0};
-		int[] lmiVal = new int[1] {1};
+		// int[] rmiVal = new int[1] {0};
+		// int[] lmiVal = new int[1] {1};
+
+		int maxSpawn = 100;
+		int vMax = 5;
+		int vDurationMin = 1;
+		int vDurationMax = 100;
+		int gMax = 3;
+		int gDurationMin = 1;
+		int gDurationMax = 100;
+
+		int numberOfGenes = 8;
+
+		float dupeRate = 0.05f;
+		float muteRate = 0.05f;
+		// .05
+		float delRate = 0.01f;
+		float changePercent = 0.15f;
+		genome testGenome = new genome();
+		testGenome.createRandomFunction();
+		// Set the genome parameters.
+		testGenome.setGenomeParameters(numberOfGenes, dupeRate, muteRate, delRate, changePercent);
+		// Create the whole genome with the provided gene params.
+		testGenome.createWholeGenome(maxSpawn, vMax, vDurationMin, vDurationMax, gMax, gDurationMin, gDurationMax);
+		// Print the contents of the genome.
+		testGenome.printGenomeContents();
+		testGenome.mutate();
+		testGenome.printGenomeContents();
+
+
+
+
+		// // Number of genes in a genom
+		// public float numGenes = 10;
 
 		// DECLARE INSTANCE OF NEURAL NETWORK.
-		neuralNetworkHandler.NeuralNetworkParameters testStruct = new neuralNetworkHandler.NeuralNetworkParameters();
+		// neuralNetworkHandler.NeuralNetworkParameters testStruct = new neuralNetworkHandler.NeuralNetworkParameters();
+		// genome testGenome = new genome();
+		// testGenome.setGenomeParameters(8, 0.05f, 0.05f, 0.01f,0.15f);
+		// testGenome.createWholeGenome(maxSpawn, vMax, vDurationMin, vDurationMax, gMax, gDurationMin, gDurationMax);
+	
 		// CALLS TO CONSTRUCTORS.
 		// (RMI, LMI, NUM_INPUT, NUM_HIDDEN, NUM_OUTPUT).
-		testStruct.setStartVariables(1,1,4,6,2);
-		testStruct.setStartingArrays(rmiVal, lmiVal);
+		// testStruct.setStartVariables(1,1,4,6,2);
+		// testStruct.setStartingArrays(rmiVal, lmiVal);
+
 		// Grab the correctly generated arrays
-		testStruct.setConnections(TESTinput_to_output,TESTinput_to_hidden,TESThidden_to_hidden, TESThidden_to_output, TESToutput_to_hidden);
+		// testStruct.setConnections(TESTinput_to_output,TESTinput_to_hidden,TESThidden_to_hidden, TESThidden_to_output, TESToutput_to_hidden);
 
 		// genome testGenome = new genome();
 		// testGenome.setGenomeVariables(8);
@@ -357,6 +462,9 @@ public class genomeHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+
 		// makeGenome(8);
 		// distance(100,100, 100,100);
 		// Debug.Log("WE MADE it!");
