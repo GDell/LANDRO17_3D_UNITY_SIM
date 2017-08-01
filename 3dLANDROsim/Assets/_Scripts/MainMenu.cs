f434862499baeb0c3073651845fc1202f0ce2399
Gabriel <gabriel.dellaccio@gmail.com>
2017-08-01 10:43:38 -0400

    Added basic input fields to main menu

--- 3dLANDROsim/Assets/_Scripts/MainMenu.cs
+++ 3dLANDROsim/Assets/_Scripts/MainMenu.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+	public InputField numIndInput;
+	public InputField avgGeneInput;
+	public InputField numGenerations;
+	// // public InputField numIndInput = GameObject.Find("Number_of_Individuals").GetComponent<InputField>();
+
+
+	public int INPUTnumberOfIndividuals;
+	public int INPUTavgGenomeSize;
+	public int INPUTnumberOfGenerations;
 	// Use this for initialization
 	void Start () {
+		INPUTnumberOfGenerations = 20;
+		INPUTavgGenomeSize = 10;
+		INPUTnumberOfIndividuals = 10;
 	}
 	
 	// Update is called once per frame
@@ -14,6 +27,34 @@ public class MainMenu : MonoBehaviour {
 	}
 
 	public void StartSimulation() {
+
+		INPUTnumberOfIndividuals = int.Parse(numIndInput.text);
+		INPUTavgGenomeSize = int.Parse(avgGeneInput.text);
+		INPUTnumberOfGenerations = int.Parse(numGenerations.text);
+
+
+		// if (avgGeneInput.Text == null) {
+		// 	INPUTavgGenomeSize = 10;
+		// 	Debug.Log("Average genome size set to default 10");
+		// } else {
+		// 	INPUTavgGenomeSize = Int32.TryParse(avgGeneInput.text);
+		// }
+
+		// if (numIndInput.Text == null) {
+		// 	INPUTnumberOfIndividuals = 20;
+		// 	Debug.Log("Average generation size set to default 20");
+		// } else {
+		// 	INPUTnumberOfIndividuals = Int32.TryParse(numIndInput.text);
+		// }
+
+
+		// if (numGenerations.Text == null) {
+		// 	INPUTnumberOfGenerations = 20;
+		// 	Debug.Log("Number of generations set to default 10");
+		// } else {
+		// 	INPUTnumberOfGenerations = Int32.TryParse(numIndInput.text);
+		// }
+
 		SceneManager.LoadScene("Experiment");
 		Main.pastStartScreen = true;
 	}
